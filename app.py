bf317bf7bf126ce62e7f1d9732c42f12462ef278
Tapas <tapasparida7077@gmail.com>
2025-08-14 18:22:49 +0530

    Remove nltk_data.py and update app.py for Streamlit

--- app.py
+++ app.py
@@ -15,11 +15,12 @@ nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
 if not os.path.exists(nltk_data_dir):
     os.makedirs(nltk_data_dir)
 
-nltk.download('stopwords', download_dir=nltk_data_dir)
-nltk.download('punkt', download_dir=nltk_data_dir)
-
 # Tell NLTK to look in this directory
 nltk.data.path.append(nltk_data_dir)
+
+# Download required datasets if not present
+nltk.download('stopwords', download_dir=nltk_data_dir)
+nltk.download('punkt', download_dir=nltk_data_dir)
 
 # ---------------------------
 # Text preprocessing
