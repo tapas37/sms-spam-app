0c379194aa5b8902c63f78d32c1f5d74c7423bbe
Tapas <tapasparida7077@gmail.com>
2025-08-14 21:30:06 +0530

    Fix NLTK LookupError for Streamlit Cloud

--- app.py
+++ app.py
@@ -2,34 +2,43 @@ import streamlit as st
 import pickle
 import string
 import nltk
-import os
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-ps = PorterStemmer()
+# ---------------------------
+# Download NLTK data at runtime (for Streamlit Cloud)
+# ---------------------------
+nltk.download('punkt')
+nltk.download('stopwords')
 
-# ---------------------------
-# Setup NLTK data for Streamlit Cloud
-# ---------------------------
-nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
-nltk.data.path.append(nltk_data_dir)
+ps = PorterStemmer()
 
 # ---------------------------
 # Text preprocessing
 # ---------------------------
 def transform_text(text):
+    # lower case
     text = text.lower()
-    text = nltk.word_tokenize(text)  # will use local punkt
+
+    # tokenization
+    text = nltk.word_tokenize(text)
+
+    # remove non-alphanumeric
     y = [i for i in text if i.isalnum()]
     text = y[:]
     y.clear()
+
+    # remove stopwords and punctuation
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
     text = y[:]
     y.clear()
+
+    # stemming
     for i in text:
         y.append(ps.stem(i))
+
     return " ".join(y)
 
 # ---------------------------
@@ -42,10 +51,18 @@ model = pickle.load(open('model.pkl', 'rb'))
 # Streamlit app
 # ---------------------------
 st.title("SMS Spam Classifier")
+
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
+    # 1. Preprocess
     transformed_sms = transform_text(input_sms)
+
+    # 2. Vectorize
     vector_input = tfidf.transform([transformed_sms])
+
+    # 3. Predict
     result = model.predict(vector_input)[0]
+
+    # 4. Display
     st.header("Spam" if result == 1 else "Not Spam")
