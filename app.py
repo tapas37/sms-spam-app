4143cf8ab645f06b896839a4803b9d4277c7001e
Tapas <tapasparida7077@gmail.com>
2025-08-14 22:16:06 +0530

    Remove NLTK, use manual preprocessing

--- app.py
+++ app.py
@@ -1,45 +1,53 @@
 import streamlit as st
 import pickle
 import string
-import os
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
 
 # ---------------------------
-# Setup NLTK to use the local nltk_data folder
+# Manual stopwords list
 # ---------------------------
-nltk_data_dir = os.path.join(os.path.dirname(__file__), "nltk_data")
-nltk.data.path.append(nltk_data_dir)
+STOPWORDS = set("""
+a about above after again against all am an and any are aren't as at be because been
+before being below between both but by can't cannot could couldn't did didn't do does
+doesn't doing don't down during each few for from further had hadn't has hasn't have
+haven't having he he'd he'll he's her here here's hers herself him himself his how
+how's i i'd i'll i'm i've if in into is isn't it it's its itself let's me more most
+mustn't my myself no nor not of off on once only or other ought our ours ourselves out
+over own same shan't she she'd she'll she's should shouldn't so some such than that
+that's the their theirs them themselves then there there's these they they'd they'll
+they're they've this those through to too under until up very was wasn't we we'd we'll
+we're we've were weren't what what's when when's where where's which while who who's
+whom why why's with won't would wouldn't you you'd you'll you're you've your yours
+yourself yourselves
+""".split())
 
-ps = PorterStemmer()
+# ---------------------------
+# Simple stemmer function
+# ---------------------------
+def simple_stem(word):
+    suffixes = ['ing', 'ly', 'ed', 's', 'es', 'ment']
+    for suffix in suffixes:
+        if word.endswith(suffix) and len(word) > len(suffix) + 2:
+            return word[:-len(suffix)]
+    return word
 
 # ---------------------------
 # Text preprocessing
 # ---------------------------
 def transform_text(text):
+    # lowercase
     text = text.lower()
 
-    # tokenization using local punkt
-    text = nltk.word_tokenize(text)
+    # basic tokenization
+    tokens = text.split()
 
-    # remove non-alphanumeric
-    y = [i for i in text if i.isalnum()]
-    text = y[:]
-    y.clear()
+    # remove punctuation and stopwords
+    tokens = [word.strip(string.punctuation) for word in tokens]
+    tokens = [word for word in tokens if word not in STOPWORDS and word]
 
-    # remove stopwords and punctuation using local stopwords
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-    text = y[:]
-    y.clear()
+    # simple stemming
+    tokens = [simple_stem(word) for word in tokens]
 
-    # stemming
-    for i in text:
-        y.append(ps.stem(i))
-
-    return " ".join(y)
+    return " ".join(tokens)
 
 # ---------------------------
 # Load model and vectorizer
