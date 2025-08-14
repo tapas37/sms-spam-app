e87123a52004245e6d7e8b622ebdcedce6833be8
Tapas <tapasparida7077@gmail.com>
2025-08-14 18:04:08 +0530

    Fix NLTK LookupError for Streamlit Cloud

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 import streamlit as st
 import pickle
-from nltk.corpus import stopwords
 import string
 import nltk
+import os
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-ps=PorterStemmer()
-import nltk
 
-# Download required NLTK datasets
-nltk.download('stopwords')
-nltk.download('punkt')
+ps = PorterStemmer()
 
+# ---------------------------
+# Setup NLTK data for Streamlit Cloud
+# ---------------------------
+nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
+if not os.path.exists(nltk_data_dir):
+    os.makedirs(nltk_data_dir)
+
+nltk.download('stopwords', download_dir=nltk_data_dir)
+nltk.download('punkt', download_dir=nltk_data_dir)
+
+# Tell NLTK to look in this directory
+nltk.data.path.append(nltk_data_dir)
+
+# ---------------------------
+# Text preprocessing
+# ---------------------------
 def transform_text(text):
     # lower case
     text = text.lower()
@@ -18,15 +31,12 @@ def transform_text(text):
     # tokenization
     text = nltk.word_tokenize(text)
 
-    # removing special charecter
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
+    # remove non-alphanumeric
+    y = [i for i in text if i.isalnum()]
     text = y[:]
     y.clear()
 
-    # removing special charecter and punctuation
+    # remove stopwords and punctuation
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
@@ -39,31 +49,31 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf=pickle.load(open('vectorizer.pkl','rb'))
-model=pickle.load(open('model.pkl','rb'))
+# ---------------------------
+# Load model and vectorizer
+# ---------------------------
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
+# ---------------------------
+# Streamlit app
+# ---------------------------
 st.title("SMS Spam Classifier")
 
-input_sms=st.text_area("Enter the message")
+input_sms = st.text_area("Enter the message")
 
-if st.button('predict'):
+if st.button('Predict'):
+    # 1. Preprocess
+    transformed_sms = transform_text(input_sms)
 
+    # 2. Vectorize
+    vector_input = tfidf.transform([transformed_sms])
 
+    # 3. Predict
+    result = model.predict(vector_input)[0]
 
-
-    # 1. preprocess
-
-    transformed_sms=transform_text(input_sms)
-
-    # 2. vectorize
-
-    vector_input=tfidf.transform([transformed_sms])
-    # 3. predict
-    result=model.predict(vector_input)[0]
-    # 4. display
-
-    if result==1:
-        st.header("spam")
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
     else:
-        st.header("Not spam")
-
+        st.header("Not Spam")
